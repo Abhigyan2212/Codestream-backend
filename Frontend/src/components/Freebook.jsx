5f2fefeea04836cd935b61e11f47fd20e4a63fa8
Abhigyan Shrivastava <97295401+Abhigyan2212@users.noreply.github.com>
2024-06-29 17:38:17 +0530

    app created

---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import axios from "axios";
+import Cards from "./Cards";
+
+function Freebook() {
+  const [book, setBook] = useState([]);
+
+  useEffect(() => {
+    const getBook = async () => {
+      try {
+        const res = await axios.get("/book");
+        const data = res.data.filter((data) => data.category === "Free");
+        console.log(data);
+        setBook(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getBook();
+  }, []);
+
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <>
+      <div className="max-w-screen-2xl container mx-auto md:px-20 px-4">
+        <div className="text-center">
+          <h1 className="font-semibold text-3xl pb-4">
+            Free Offered Handwritten Notes
+          </h1>
+          <p></p>
+        </div>
+
+        <div>
+          <Slider {...settings}>
+            {book.map((item) => (
+              <Cards item={item} key={item.id} />
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Freebook;
---
+++ index.js
@@ -0,0 +1,49 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import path from "path"
+
+import bookRoute from "./route/book.route.js";
+import userRoute from "./route/user.route.js"
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
+
+// connect to mongoDB
+try {
+    mongoose.connect(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    console.log("Connected to mongoDB");
+} catch (error) {
+    console.log("Error: ", error);
+}
+
+// defining routes
+app.use("/book", bookRoute);
+app.use("/user", userRoute);
+
+
+//deployment
+
+if(process.env.NODE_ENV === "production"){
+    const dirPath = path.resolve();
+    app.use(express.static("Frontend/dist"));
+    app.get("*",(req,res) =>{
+        res.sendFile(path.resolve(dirPath,"Frontend","dist","index.html"));
+  })
+    
+}
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
